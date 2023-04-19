57760c8684fec05d6fdbeda3efd969b855f3831f
Agry88 <rgok307085@gmail.com>
2023-04-19 18:18:12 +0800

    Feat: add ouath endpoint headers.

--- src/pages/api/oauth/index.ts
+++ src/pages/api/oauth/index.ts
@@ -4,29 +4,30 @@ import { hostURL } from "@/config"
 import z from "zod";
 import getEnvironmentVariable from "@/utils/getEnvironmentVariable";
 
-const NextRequestSchema = z.object({
-  query: z.object({
-    code: z.string(),
-  }),
-})
-
 const AxiosResponseSchema = z.object({
   access_token: z.string(),
   user_id: z.string(),
 })
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { query } = NextRequestSchema.parse(req);
 
-  const { code } = query;
+  const { code } = req.query;
 
   const oauthRes = await axios.post("https://api.instagram.com/oauth/access_token", {
     client_id: getEnvironmentVariable("NEXT_PUBLIC_INSTAGRAM_CLIENT_ID", ""),
     client_secret: getEnvironmentVariable("INSTAGRAM_CLIENT_SECRET", ""),
     code: code,
     grant_type: "authorization_code",
-    redirect_uri: `${hostURL}/api/oauth`,
+    redirect_uri: `${hostURL}/api/oauth/`,
+  }, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    }
+  }).catch((err) => {
+    console.error(err);
+    res.status(500).send("Something went wrong");
   })
+  
 
   const { access_token: accessToken, user_id: userId } = AxiosResponseSchema.parse(oauthRes);
   res.redirect(`${hostURL}/redirect?accessToken=${accessToken}&userId=${userId}`);
